d797e253449c59f56deb1150b35757b2f41c8101
VinayRana3363 <73163662+VinayRana3363@users.noreply.github.com>
2020-11-05 10:01:35 +0530

    Program: Add multiple files for Intro to Mongoose and promises(40010) (#13)

--- src/Server.ts
+++ src/Server.ts
@@ -3,6 +3,7 @@ import * as bodyparser from 'body-parser';
 import { notFoundHandler, errorHandler } from './libs/routes';
 
 import mainRouter from './router';
+import Databse from './libs/Database';
 
 
 class Server {
@@ -44,12 +45,26 @@ class Server {
     }
 
     public run() {
-        const { PORT, NODE_ENV } = this.config;
+        const { PORT, NODE_ENV , MONGO_URL } = this.config;
 
-        this.app.listen(PORT, () => {
-            const message = `|| App is running at port '${PORT}' in '${NODE_ENV}' mode ||`;
-            console.log(message);
+        Databse.open(MONGO_URL)
+            .then((res) => {
+                console.log('Succesfully connect with MongoDB');
+                this.app.listen(PORT, () => {
+                    const message = `|| App is running at port '${PORT}' in '${NODE_ENV}' mode ||`;
+                    console.log(message);
+                });
+            })
+            .catch((err) => console.log(err));
+
+        Databse.close(MONGO_URL, (err) => {
+            if (err) {
+                console.log('error occured', err);
+                return;
+            }
+            console.log('Succesfully disconnect with MongoDB');
         });
+        return this;
     }
 }
 
