0e5bdce572f3d326625eab0f4d92a4e5563892a5
VinayRana3363 <73163662+VinayRana3363@users.noreply.github.com>
2020-12-01 18:01:48 +0530

    Update seedData.ts

--- src/libs/seedData.ts
+++ src/libs/seedData.ts
@@ -1,13 +1,8 @@
-import * as bcrypt from 'bcrypt';
 import UserRepository from '../repositories/user/UserRepository';
 import config from '../config/configuration';
 
 const userRepository: UserRepository = new UserRepository();
 console.log(config.pass, typeof(config.pass));
-// const salt = bcrypt.genSaltSync(10);
-// const hash = bcrypt.hashSync(config.pass, salt);
-
-// const hash2 = bcrypt.hashSync(config.pass2, salt);
 
 export default async() => {
     await userRepository.count({})
